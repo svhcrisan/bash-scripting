904deb24167ca0b60cbdde52252e801dc0b2a56f
Horea Crisan <horea.crisan@softvision.ro>
2019-12-04 16:02:14 +0200

    modified ex3

--- section-3-exercises/ex3.sh
+++ section-3-exercises/ex3.sh
@@ -1,10 +1,15 @@
 cat /etc/shadow
 
-if [ $? -eq "0" ]
-then
-    echo "Command succeeded"
-elif [ $? -ne "0" ]
-then
-    echo "Command failed"
-    exit "1"
-fi
+function yoo() {
+    echo "A function was called..."
+    if [ $? -eq "0" ]
+    then
+        echo "Command succeeded"
+    elif [ $? -ne "0" ]
+    then
+        echo "Command failed"
+        exit "1"
+    fi
+}
+
+yoo
